7b189eb5453fee2257d1f883048049f8bd6fa4a3
rmarro <rmarro47@gmail.com>
2017-12-15 22:03:42 -0600

    added timer for question and logic for if time runs out, answer wrong, or answer right

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,27 @@
+// Press start
 // Start timer counting down from 30
 // A question shows up
 // 4 possible options show up
-   // Right answer should be in different positions each time
 // If right answer is clicked
-   // 5 second timer starts
-   // You got it right, show picture, correct++
+    // 5 second timer starts
+    // You got it right, show picture, correct++
 // If wrong answer is clicked
-  // 5 second timer starts
-  // You got it wrong, show correct answer, show picture, wrong++
+    // 5 second timer starts
+    // You got it wrong, show correct answer, show picture, wrong++
 // If time runs out
-  // 5 second timer starts
-  // You ran out of time, show correct answer, show picture, unanswered++
-// When 5 second timer runs out, new question and options are shown
-// When 5 questions have been completed, show final summary screen
+    // 5 second timer starts
+    // You ran out of time, show correct answer, show picture, unanswered++
+// When 5 second timer runs out 
+    // 30 second timer starts again
+    // new question and options are shown
+    // round++
+// When 5 rounds have been completed, show final summary screen
 
 var questionList = [ 
     {
-      question: "What is the right answer?",
-      options: ["some answer", "another answer", "third answer", "last answer"],
-      rightAnswer: 0
+        question: "What is the right answer?",
+        options: ["some answer", "another answer", "third answer", "last answer"],
+        rightAnswer: 0
     },
     {
         question: "What is the right answer2?",
@@ -30,20 +33,48 @@ var questionList = [
         options: ["some answer3", "another answer3", "third answer3", "last answer3"],
         rightAnswer: 2
       }
-    ]
-    
-// Randomly select one of the indexes in questionList
-var qIndex = Math.floor(Math.random()*questionList.length);
-// Save the object with that index as qSet
-var qSet = questionList[qIndex];
+    ];
 
-// Show the question from qSet
+var qSet;
+var round = 0;
+var countdown;
+var wrong = 0;
+var right = 0;
+var unanswered = 0;
+
+// 30 second timer
+function timer () {
+    timeLeft = 30;
+    $("#timer").text("Seconds left: " + timeLeft);
+    countdown = setInterval(showTime, 1000);
+
+    function showTime() {
+        $("#timer").text("Seconds left: " + timeLeft);
+        if (timeLeft === 0) {
+            clearInterval(countdown);
+            $("#question-full").empty();
+            showAnswer();
+            $("#message").text("Time's up!")
+        } else
+        timeLeft--;
+    };
+};
+
+
+// Get a random question/answer set from list
+function getqSet() {
+    // Randomly select one of the indexes in questionList
+    var qIndex = Math.floor(Math.random()*questionList.length);
+    // Save the object with that index as qSet
+    qSet = questionList[qIndex];
+}
+
+// Show the question from randomly selected qSet
 function showQuestion() {
     var currentQuestion = $("<div>");
     currentQuestion.text(qSet.question);
     $("#question-full").append(currentQuestion);
 }
-showQuestion();
 
 // Show the options from qSet
 function showOptions() {
@@ -54,15 +85,43 @@ function showOptions() {
     $("#question-full").append(currentOptions);
     }
 }
-showOptions();
 
-// Checks if correct answer was clicked by comparing value attribute (option index) to correctAnswer (correct option index)
-$(".option").on("click", function() {
-    if (+$(this).val() === qSet.rightAnswer) {
-        console.log("correct");
-    } else {
-        console.log("incorrect");
-    }
-})
+// Show the correct answer from qSet // ADD PICTURE!!
+function showAnswer () {
+    var currentAnswer = $("<div>");
+    currentAnswer.text("Correct answer: " + qSet.options[qSet.rightAnswer]);
+    $("#answer-full").append(currentAnswer);
+}
 
-// $("#question-full").empty(); //empties out div-- can use when showing answer and final page
+// set timer, show
+function gameplay() {
+    round++;
+    $("#rounds").text("Round " + round + " of 5");
+    timer();
+    getqSet();
+    showQuestion();
+    showOptions();
+  
+  // Checks if correct answer was clicked by comparing value attribute (option index) to correctAnswer (correct option index)
+    $(".option").on("click", function() {
+        clearInterval(countdown);
+        $("#question-full").empty();
+        showAnswer();
+        //if right
+        if (+$(this).val() === qSet.rightAnswer) {
+            $("#message").text("You got it!")
+        } 
+        //if wrong
+        else {
+            $("#message").text("Nope!")
+        }
+    })
+};
+
+
+gameplay();
+
+
+// make a 5 second countdown function that calls gameplay again at the 
+// end of it until round 5 is done, then it shows the final page and
+// call this in the gameplay onclick AND the showTime function if 0
